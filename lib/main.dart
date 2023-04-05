b23ce64801b75c9eaba3b61274322de2940bf7c6
Roviil <qowjdgns0106@naver.com>
2023-04-06 00:50:21 +0900

    메인 수정

--- lib/main.dart
+++ lib/main.dart
@@ -83,40 +83,6 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => PartyBoardScreen()),
-                );
-              },
-              child: Text(
-                '구인구직 게시판',
-                style: TextStyle(fontSize: 20),
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 20.0),
-                backgroundColor: Color(0xffC1D3FF),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FreeBoardScreen()),
-                );
-              },
-              child: Text(
-                '자유게시판',
-                style: TextStyle(fontSize: 20),
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 20.0),
-                backgroundColor: Color(0xffC1D3FF),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
                   MaterialPageRoute(builder: (context) => LoginPage()),
                 );
               },
